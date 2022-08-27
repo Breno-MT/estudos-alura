08c3ef8bfaae0ee86d2d9c044485f6a78ede6f1e
Breno-MT <hentgenp@gmail.com>
2022-08-27 15:02:17 -0300

    [BRENO] Init commit

---
+++ estudos-vuejs3/estudos-vuejs3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <HelloWorld />
+
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld
+  }
+});
+
+</script>
---
+++ estudos-vuejs3/estudos-vuejs3/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Olá mundo!</h1>
+    <h2>Estudos Alura em Vuejs 3 !!!!!</h2>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "HelloWorld"
+})
+
+</script>
+
+<style scope>
+
+h1 {
+    color: blue;
+}
+
+</style>
