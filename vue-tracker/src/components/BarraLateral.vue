fd2b40b0d5d6596111ec9eb2365715a5e993fa16
Breno-MT <hentgenp@gmail.com>
2022-08-29 14:01:11 -0300

    [BRENO] Commit project

---
+++ vue-tracker/src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "BarraLateral"
+})
+
+</script>
+
+<template>
+    <header>
+        <img src="../assets/logo.png" alt="">
+    </header>
+
+</template>
+
+
+<style scoped>
+
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+
+</style>
