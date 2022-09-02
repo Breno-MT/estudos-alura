a183a4e1bcaeb49341998b1a3d3a6ed7aa637222
Breno-MT <hentgenp@gmail.com>
2022-09-02 19:54:46 -0300

    [BRENO] Update

--- vue-tracker/src/router/index.ts
+++ vue-tracker/src/router/index.ts
@@ -1,18 +1,28 @@
 import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from '../views/Projetos/Formulario.vue'
 
 const rotas: RouteRecordRaw[] = [
     {
-    path: '/',
-    name: 'Tarefas',
-    component: Tarefas
+        path: '/',
+        name: 'Tarefas',
+        component: Tarefas
     },
-
     {
-    path: '/projetos',
-    name: 'Projetos',
-    component: Projetos
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
+    },
+    {
+        path: '/projetos/novo',
+        name: 'Novo Projeto',
+        component: Formulario
+    },
+    {
+        path: '/projetos/:id',
+        name: 'Editar Projeto',
+        component: Formulario
     }
 
 ];
